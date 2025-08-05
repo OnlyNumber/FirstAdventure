b75e6fdd9ab9370d8513f122b7611485e727204e
OnlyNumber <98099864+OnlyNumber@users.noreply.github.com>
2025-08-05 13:10:46 +0300

    Added fast stop

--- Adventure/Assets/Scripts/FastCasino.cs
+++ Adventure/Assets/Scripts/FastCasino.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class FastCasino : MonoBehaviour
 {
-    public float speed = 10;
+    public const float Ymin = -300;
+    public const float Ymax = 500;
+    public const float DistanceBetweenSlots = 100;
+
+    public const float SpinningSpeed = 3600;
+    public const float SmoothSpeed = 400;
+
+    public float CurrentSpeed = 3600;
 
     public List<RectTransform> Spins = new();
 
-    public const float Ymin = -300;
-    public const float Ymax = 500;
+    public float randomResult = 0;
+
+    public bool IsStopped = false;
 
     private void Update()
     {
+        if (IsStopped)
+            return;
+
         foreach (var item in Spins)
         {
             if (item.transform.localPosition.y <= Ymin)
@@ -22,9 +33,48 @@ public class FastCasino : MonoBehaviour
                 item.transform.localPosition = Vector3.up * (Ymax + yPos); 
             }
 
-            item.transform.localPosition = new Vector3(0, item.transform.localPosition.y - speed * Time.deltaTime, 0);
+            item.transform.localPosition = new Vector3(0, item.transform.localPosition.y - CurrentSpeed * Time.deltaTime, 0);
         }
+    }
 
+    public void GoAgain()
+    {
+        CurrentSpeed = SpinningSpeed;
+
+        IsStopped = false;
+    }
+
+    [ContextMenu("GetRandomResultSharp")]
+    public void GetRandomResultSharp()
+    {
+        randomResult = Random.Range(0, 4);
+
+        Spins[0].transform.localPosition = Vector3.up * DistanceBetweenSlots * randomResult;
+
+        if (randomResult == 0)
+            Spins[1].transform.localPosition = Vector3.up * (Ymax - DistanceBetweenSlots);
+        else
+            Spins[1].transform.localPosition = Vector3.up * (Ymin + DistanceBetweenSlots * (randomResult - 1) );
+
+
+        IsStopped = true;
+            
+    }
+
+    [ContextMenu("GetRandomResultSmooth")]
+    public void GetRandomResultSmooth()
+    {
+        randomResult = Random.Range(0, 4);
+
+        CurrentSpeed = SmoothSpeed;
+
+        
+
+    }
+
+    public IEnumerator SmoothStop()
+    {
+        yield return new WaitForSeconds(0);
     }
 
 }
